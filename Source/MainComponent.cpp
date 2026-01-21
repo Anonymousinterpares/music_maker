fa07d4836c1c1ff7b7e3eb91ac9fb0aed6edfba6
Anonymousinterpares <piotr.sobczynski@gmail.com>
2026-01-21 22:58:41 +0100

    virtual piano implemented

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -2,80 +2,75 @@
 
 MainComponent::MainComponent()
 {
-    juce::Logger::writeToLog ("--- MainComponent Constructor Starting ---");
-
     addAndMakeVisible (nativeBtn);
-    nativeBtn.setButtonText ("NATIVE C++ BEEP (FALLBACK)");
-    nativeBtn.onClick = [this] {
-        isPlayingTestSound = !isPlayingTestSound;
-        nativeBtn.setColour (juce::TextButton::buttonColourId, isPlayingTestSound.load() ? juce::Colours::red : juce::Colours::darkgrey);
-    };
+    nativeBtn.setButtonText ("PLAY C4 (NATIVE)");
+    nativeBtn.onClick = [this] { synth.noteOn (1, 60, 0.8f); };
+
+    // Setup 8 voices for polyphony
+    for (int i = 0; i < 8; ++i)
+    {
+        auto* voice = new SynthVoice();
+        synth.addVoice (voice);
+    }
+    synth.addSound (new SynthSound());
 
     auto userDataPath = juce::File ("C:\\music_maker\\WebViewData");
-    if (!userDataPath.exists()) userDataPath.createDirectory();
-
-    auto options = juce::WebBrowserComponent::Options{}.withBackend (juce::WebBrowserComponent::Options::Backend::webview2).withWinWebView2Options (juce::WebBrowserComponent::Options::WinWebView2{}.withUserDataFolder (userDataPath)).withNativeIntegrationEnabled (true).withNativeFunction ("testSound", [this] (const juce::Array<juce::var>&, auto completion) {
-            isPlayingTestSound = !isPlayingTestSound;
-            juce::Logger::writeToLog ("BRIDGE SUCCESS: testSound called!");
+    auto options = juce::WebBrowserComponent::Options{}
+        .withBackend (juce::WebBrowserComponent::Options::Backend::webview2)
+        .withWinWebView2Options (juce::WebBrowserComponent::Options::WinWebView2{}.withUserDataFolder (userDataPath))
+        .withNativeIntegrationEnabled (true)
+        .withNativeFunction ("playNote", [this] (const juce::Array<juce::var>& params, auto completion) {
+            if (params.size() >= 2)
+            {
+                int note = params[0];
+                float vel = (float)params[1];
+                if (vel > 0) synth.noteOn (1, note, vel);
+                else        synth.noteOff (1, note, 0.0f, true);
+            }
             completion (true);
-        }).withEventListener ("toggleSoundEvent", [this] (juce::var) {
-            isPlayingTestSound = !isPlayingTestSound;
-            juce::Logger::writeToLog ("BRIDGE SUCCESS: toggleSoundEvent triggered!");
-        }).withResourceProvider ([this] (const juce::String& url) -> std::optional<juce::WebBrowserComponent::Resource> {
-            std::vector<std::byte> data;
-            data.assign ((const std::byte*) BinaryData::index_html, (const std::byte*) BinaryData::index_html + BinaryData::index_htmlSize);
-            return juce::WebBrowserComponent::Resource { std::move (data), "text/html" };
+        })
+        .withEventListener ("playNoteEvent", [this] (juce::var params) {
+            int note = params["note"];
+            float vel = params["velocity"];
+            if (vel > 0) synth.noteOn (1, note, vel);
+            else        synth.noteOff (1, note, 0.0f, true);
+        })
+        .withResourceProvider ([this] (const juce::String&) -> std::optional<juce::WebBrowserComponent::Resource> {
+            return juce::WebBrowserComponent::Resource { 
+                { (const std::byte*) BinaryData::index_html, (const std::byte*) BinaryData::index_html + BinaryData::index_htmlSize },
+                "text/html" 
+            };
         }, juce::WebBrowserComponent::getResourceProviderRoot());
 
     webBrowser = std::make_unique<juce::WebBrowserComponent> (options);
     addAndMakeVisible (*webBrowser);
-    
     webBrowser->goToURL (juce::WebBrowserComponent::getResourceProviderRoot());
 
     setSize (1000, 700);
     setAudioChannels (0, 2);
-    juce::Logger::writeToLog ("--- MainComponent Constructor Finished ---");
 }
 
-MainComponent::~MainComponent()
-{
-    shutdownAudio();
-}
+MainComponent::~MainComponent() { shutdownAudio(); }
 
-void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
+void MainComponent::prepareToPlay (int, double sampleRate)
 {
-    auto cyclesPerSample = 440.0 / sampleRate;
-    phaseDelta = cyclesPerSample * 2.0 * juce::MathConstants<double>::pi;
+    synth.setCurrentPlaybackSampleRate (sampleRate);
+    for (int i = 0; i < synth.getNumVoices(); ++i)
+    {
+        if (auto* v = dynamic_cast<SynthVoice*> (synth.getVoice(i)))
+            v->prepare (sampleRate);
+    }
 }
 
 void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill)
 {
-    if (isPlayingTestSound.load())
-    {
-        auto* leftBuffer  = bufferToFill.buffer->getWritePointer (0, bufferToFill.startSample);
-        auto* rightBuffer = bufferToFill.buffer->getWritePointer (1, bufferToFill.startSample);
-
-        for (auto sample = 0; sample < bufferToFill.numSamples; ++sample)
-        {
-            float sampleValue = (float)std::sin (currentPhase) * 0.125f;
-            currentPhase += phaseDelta;
-            leftBuffer[sample] = sampleValue;
-            rightBuffer[sample] = sampleValue;
-        }
-    }
-    else
-    {
-        bufferToFill.clearActiveBufferRegion();
-    }
+    bufferToFill.clearActiveBufferRegion();
+    juce::MidiBuffer incomingMidi;
+    synth.renderNextBlock (*bufferToFill.buffer, incomingMidi, bufferToFill.startSample, bufferToFill.numSamples);
 }
 
 void MainComponent::releaseResources() {}
-
-void MainComponent::paint (juce::Graphics& g)
-{
-    g.fillAll (juce::Colours::black);
-}
-
+void MainComponent::paint (juce::Graphics& g) { g.fillAll (juce::Colours::black); }
 void MainComponent::resized()
 {
     auto area = getLocalBounds();
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -2,6 +2,80 @@
 
 #include <JuceHeader.h>
 
+// A simple Synthesizer Voice
+class SynthVoice : public juce::SynthesiserVoice
+{
+public:
+    SynthVoice() {}
+
+    bool canPlaySound (juce::SynthesiserSound* sound) override { return dynamic_cast<juce::SynthesiserSound*> (sound) != nullptr; }
+
+    void startNote (int midiNoteNumber, float velocity, juce::SynthesiserSound*, int) override
+    {
+        level = velocity * 0.15f;
+        auto cyclesPerSample = juce::MidiMessage::getMidiNoteInHertz (midiNoteNumber) / getSampleRate();
+        angleDelta = cyclesPerSample * 2.0 * juce::MathConstants<double>::pi;
+        currentAngle = 0.0;
+        adsr.noteOn();
+    }
+
+    void stopNote (float, bool allowTailOff) override
+    {
+        adsr.noteOff();
+        if (!allowTailOff) clearCurrentNote();
+    }
+
+    void pitchWheelMoved (int) override {}
+    void controllerMoved (int, int) override {}
+
+    void renderNextBlock (juce::AudioBuffer<float>& outputBuffer, int startSample, int numSamples) override
+    {
+        if (!adsr.isActive()) {
+            clearCurrentNote();
+            return;
+        }
+
+        for (int sample = 0; sample < numSamples; ++sample)
+        {
+            auto envelopeValue = adsr.getNextSample();
+            
+            // Sawtooth: (2.0 * angle / 2PI) - 1.0
+            auto sampleValue = (float) (((currentAngle / juce::MathConstants<double>::pi) - 1.0) * level * envelopeValue);
+            
+            for (int channel = 0; channel < outputBuffer.getNumChannels(); ++channel)
+                outputBuffer.addSample (channel, startSample + sample, sampleValue);
+
+            currentAngle += angleDelta;
+            if (currentAngle >= juce::MathConstants<double>::twoPi)
+                currentAngle -= juce::MathConstants<double>::twoPi;
+        }
+    }
+
+    void prepare (double sampleRate)
+    {
+        adsr.setSampleRate (sampleRate);
+        juce::ADSR::Parameters params;
+        params.attack = 0.05f;
+        params.decay = 0.1f;
+        params.sustain = 0.8f;
+        params.release = 0.5f;
+        adsr.setParameters (params);
+    }
+
+private:
+    juce::ADSR adsr;
+    double currentAngle = 0.0, angleDelta = 0.0;
+    float level = 0.0f;
+};
+
+// A simple Synthesizer Sound
+struct SynthSound : public juce::SynthesiserSound
+{
+    SynthSound() {}
+    bool appliesToNote (int) override { return true; }
+    bool appliesToChannel (int) override { return true; }
+};
+
 class MainComponent  : public juce::AudioAppComponent
 {
 public:
@@ -16,15 +90,11 @@ public:
     void resized() override;
 
 private:
-    // Native fallback button
     juce::TextButton nativeBtn;
-    
-    // WebBrowserComponent must be a unique_ptr to ensure options are set before construction
     std::unique_ptr<juce::WebBrowserComponent> webBrowser;
     
-    std::atomic<bool> isPlayingTestSound { false };
-    double currentPhase = 0.0;
-    double phaseDelta = 0.0;
-
+    // Synth Core
+    juce::Synthesiser synth;
+    
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
